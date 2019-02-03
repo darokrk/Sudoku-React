e523bcd7ae428df999401de9d6c626be32876563
Dariusz Rosiek <daro.krk@interia.pl>
2019-02-03 21:17:21 +0100

    Create-react-app, delete redundant files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Sudoku game</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
