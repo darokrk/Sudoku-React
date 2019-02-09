2e1085ee04f4e580d161e4f7e781e819885a0367
Dariusz Rosiek <daro.krk@interia.pl>
2019-02-09 20:05:02 +0100

    Create TimeWatch component

---
+++ src/Components/TimeWatch/TimeWatch.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TimeWatch.scss";
+
+const TimeWatch = props => {
+  const { minutes, seconds } = props.time;
+  return (
+    <p className="timeWatch">
+      {minutes > 9 ? `${minutes}` : `0${minutes}`}:
+      {seconds > 9 ? `${seconds}` : `0${seconds}`}
+    </p>
+  );
+};
+
+export default TimeWatch;
