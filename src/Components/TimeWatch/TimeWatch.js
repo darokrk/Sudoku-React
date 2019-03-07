561dcf4115f952570768cc0152f942390a9e1706
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-07 19:29:59 +0100

    Added timeFormat function

--- src/Components/TimeWatch/TimeWatch.js
+++ src/Components/TimeWatch/TimeWatch.js
@@ -3,12 +3,14 @@ import "./TimeWatch.scss";
 
 const TimeWatch = props => {
   const { minutes, seconds } = props.time;
+  const timeFormat = (minutes, seconds) => {
+    if (seconds > 9) return `0${minutes}:${seconds}`;
+    return ` 0${minutes}:0${seconds}`;
+  };
   return (
     <p className="timeWatch">
-      {`Your Time
-      ${minutes > 9 ? `${minutes}` : `0${minutes}`}:${
-        seconds > 9 ? `${seconds}` : `0${seconds}`
-      }`}
+      Your Time
+      {timeFormat(minutes, seconds)}
       <strong>{` ${props.gameLevel}`}</strong>
     </p>
   );
