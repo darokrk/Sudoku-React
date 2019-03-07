0b0d620e9bc49b3b00946741e9d1b2a8923683c5
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-07 16:00:58 +0100

    Added display finished game time

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -3,7 +3,7 @@ import React from "react";
 const Result = props => {
   const { minutes, seconds } = props.finishedTime;
   return (
-    <li>{`${minutes > 9 ? `${minutes}` : `0${minutes}`}:${
+    <li>{`${minutes > 9 ? `${minutes}` : `0${minutes}`} : ${
       seconds > 9 ? `${seconds}` : `0${seconds}`
     }`}</li>
   );
