241d395f4a6dedcb4c837fdb14ae17ee29c6491c
Dariusz Rosiek <daro.krk@interia.pl>
2019-03-14 21:18:11 +0100

    Updates

---
+++ precache-manifest.3c1f1429d8b6861ed0a5b6cadff37716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30f06eb7cbf8fae8d9b918a021d31e00",
+    "url": "/Sudoku-React/static/media/pause.30f06eb7.png"
+  },
+  {
+    "revision": "6d289876f7c72a2023cd",
+    "url": "/Sudoku-React/static/js/runtime~main.6d289876.js"
+  },
+  {
+    "revision": "6ca79b7c91d3b9177ce3",
+    "url": "/Sudoku-React/static/js/main.6ca79b7c.chunk.js"
+  },
+  {
+    "revision": "c80cab2ffbd178de55bb",
+    "url": "/Sudoku-React/static/js/1.c80cab2f.chunk.js"
+  },
+  {
+    "revision": "6ca79b7c91d3b9177ce3",
+    "url": "/Sudoku-React/static/css/main.c118ba59.chunk.css"
+  },
+  {
+    "revision": "b2ac48a46a9608a30ccf059937f54889",
+    "url": "/Sudoku-React/index.html"
+  }
+];
